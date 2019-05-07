93f724eb3790e3b90b51d64b873d9ec199d5c615
Etrayed <etrayedyt@gmail.com>
2019-05-07 14:54:38 +0200

    Initial Commit

---
+++ src/main/java/io/github/etrayed/fileportable/FilePortable.java
@@ -0,0 +1,15 @@
+package io.github.etrayed.fileportable;
+
+/**
+ * @author Etrayed
+ */
+public class FilePortable {
+
+    FilePortable() {
+        this(null);
+    }
+
+    FilePortable(final String defaultURL) {
+
+    }
+}
---
+++ src/main/java/io/github/etrayed/fileportable/FilePortableLauncher.java
@@ -0,0 +1,18 @@
+package io.github.etrayed.fileportable;
+
+/**
+ * @author Etrayed
+ */
+public class FilePortableLauncher {
+
+    public static final String DEFAULT_URL
+            = System.getProperty("io.github.etrayed.fileportable.defaultURL", null);
+
+    public static void main(String[] args) {
+        if(DEFAULT_URL != null) {
+            
+        } else {
+
+        }
+    }
+}
